cb5e9c33aee01ab6cc20686eb52bd81039423d93
aimansulaiman <aiman.sulaiman@aymcommerce.com>
2022-05-14 22:24:04 +0800

    Putting back gallery so that it works

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
+//= link home/screenshot_gallery.png
+//= link home/safwa.png
+//= link home/mutawwif.jpg
 
 
